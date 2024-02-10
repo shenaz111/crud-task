04f9397d160448a2196c944fce8d02fbde150046
shenaz111 <shenazsheik777@gmail.com>
2024-02-10 13:26:42 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import "./App.css";
+import "./crudComponents/Components.css";
+import LandingPage from "./crudComponents/LandingPage";
+import HomePage from "./crudComponents/HomePage";
+import AddUser from "./crudComponents/AddUser";
+import UserLists from "./crudComponents/UserLists";
 function App() {
+  const navigate = useNavigate();
+  const user = [
+    {
+      name: "vimayan",
+      email: "1234@gmail.com",
+      password: "789456",
+      address: "12,street name,city name,state,country",
+    },
+  ];
+
+  const [userList, setUserList] = useState([...user]);
+  const [currentUser, setCurrentUser] = useState();
+  const [index, setIndex] = useState();
+  const EditUser = (user, index) => {
+    setCurrentUser(user);
+    setIndex(index);
+    console.log(user);
+    navigate("/adduser");
+  };
+
+  const UpdateUser = (updatedUser, id) => {
+    console.log(updatedUser);
+    const UserList = [...userList];
+    console.log(id);
+    UserList[id] = { ...updatedUser };
+    setUserList([...UserList]);
+  };
+  const Adduser = (user) => {
+    setUserList([...userList, user]);
+    console.log(userList);
+  };
+  const DeleteUser = (id) => {
+    const UserList = [...userList];
+    const newUser = UserList.filter((user, index) => index !== id);
+    setUserList(newUser);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<HomePage userList={userList} />} />
+        <Route
+          path="/adduser"
+          element={
+            <AddUser
+              currentUser={currentUser}
+              index={index}
+              UpdateUser={UpdateUser}
+              Adduser={Adduser}
+            />
+          }
+        />
+        <Route
+          path="/userlists"
+          element={
+            <UserLists
+              EditUser={EditUser}
+              userList={userList}
+              index
+              DeleteUser={DeleteUser}
+            />
+          }
+        />
+      </Routes>
     </div>
   );
 }
